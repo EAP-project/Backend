2fce4ca6faef6d6c53798973ca504df9891bd708
vijayanga <vijayanga123@gmail.com>
2025-09-28 17:51:55 +0530

    setup initial version

---
+++ src/main/java/com/automobileproject/EAP/EapApplication.java
@@ -0,0 +1,13 @@
+package com.automobileproject.EAP;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EapApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EapApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/automobileproject/EAP/EapApplicationTests.java
@@ -0,0 +1,13 @@
+package com.automobileproject.EAP;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EapApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
